6dac456d6e6ab550aaf42e92cdcfd1de8fee968b
Melquias Ribeiro <melquias10.mr@gmail.com>
2021-02-05 16:01:31 -0300

    feat: dahsboard  page initial style

---
+++ src/components/header/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { BiLogOut } from 'react-icons/bi';
+import Logo from '../../assets/images/Mateus.png';
+import { Container, Cart } from './styles';
+
+export default function Header() {
+  return (
+    <Container>
+      <Link to="/dashboard">
+        <img src={Logo} alt="logo" />
+      </Link>
+      <Cart to="/login">
+        <div>
+          <strong>Sair</strong>
+        </div>
+        <BiLogOut size={36} color="#fff" />
+      </Cart>
+    </Container>
+  );
+}
--- src/pages/dahsboard/index.js
+++ src/pages/dahsboard/index.js
@@ -1,10 +1,63 @@
 import React from 'react';
-import { Wrapper } from './styles';
+import { MdDelete } from 'react-icons/md';
+import { AiFillEdit } from 'react-icons/ai';
+
+import {
+    Wrapper,
+    ListContainer,
+    ListItem,
+    IconsContainer,
+    RigthSideContainer,
+} from './styles';
+import Header from '../../components/header';
+import User from '../../assets/images/user.svg';
 
 export default function Dashboard() {
+    const users = [
+        {
+            picture: User,
+            name: 'melquias Rosa Ribeiro',
+            email: 'melquias10.mr@gmail.com',
+            phone: '(98)981877537',
+        },
+        {
+            picture: User,
+            name: 'melquias Rosa Ribeiro',
+            email: 'melquias10.mr@gmail.com',
+            phone: '(98)981877537',
+        },
+        {
+            picture: User,
+            name: 'melquias Rosa Ribeiro',
+            email: 'melquias10.mr@gmail.com',
+            phone: '(98)981877537',
+        },
+    ];
+
     return (
-        <Wrapper>
-            <h1>Dash</h1>
-        </Wrapper>
+        <>
+            <Header />
+            <Wrapper>
+                <h1>Lista de Usuários</h1>
+                <ListContainer>
+                    {users.map((user) => (
+                        <ListItem>
+                            <img src={user.picture} alt="user.name" />
+                            <RigthSideContainer>
+                                <div>
+                                    <strong>{user.name}</strong>
+                                    <p>{user.email}</p>
+                                    <p>{user.phone}</p>
+                                </div>
+                                <IconsContainer>
+                                    <MdDelete color="#F63E37" size={32} />
+                                    <AiFillEdit size={32} />
+                                </IconsContainer>
+                            </RigthSideContainer>
+                        </ListItem>
+                    ))}
+                </ListContainer>
+            </Wrapper>
+        </>
     );
 }
